fdb2b56bb91eb8a89f33727c958ddaa6b1df8758
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-07 20:41:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,13 +1,13 @@
-def using_push
-
+def using_push (array, element)
+  array.push(element)
 end
 
-def using_unshift
-
+def using_unshift(array, element)
+  array.unshift(element)
 end
 
-def using_pop
-
+def using_pop(array)
+  array.pop
 end
 
 def pop_with_args
